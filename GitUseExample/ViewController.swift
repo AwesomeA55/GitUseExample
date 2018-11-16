a5de6049e6bcf5b7e72e605e969d58ec4e19c038
Gopisetty, Ayush <166235@s020rmc148-17.local>
2018-11-16 09:15:34 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Gopisetty, Ayush on 11/16/18.
+//  Copyright © 2018 Ayush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
